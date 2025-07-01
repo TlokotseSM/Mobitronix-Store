705b7415c4f9dc9ced6554d07d353267def28631
TlokotseSM <tlokotsemogudi@gmail.com>
2025-07-01 14:19:12 +0200

    Updated MongoDB setup

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,24 @@
 import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
+app.get("/products", (req,res) => {
+    // res.send("Server is ready");
+})
+
+
+
 app.listen(5000, () => {
+    connectDB();
     console.log('Server Started at http://localhost:5000')
 })
+
+
+
+//password: ZqU3z4qriV23kTwj
+
+//mongodb+srv://tlokotsemogudi:ZqU3z4qriV23kTwj@cluster0.4xheq9c.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
