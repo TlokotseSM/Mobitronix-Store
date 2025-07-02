c14283723fe4368ceabe1d8f9e52dbe27828c16d
TlokotseSM <tlokotsemogudi@gmail.com>
2025-07-02 12:24:45 +0200

    added product api's on my sever

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from "dotenv";
 import { connectDB } from './config/db.js';
 import Product from './models/product.model.js';
+import mongoose from 'mongoose';
 
 
 dotenv.config();
@@ -9,6 +10,17 @@ dotenv.config();
 const app = express();
 
 app.use(express.json()); //allows us to accept json data in the req.body
+
+app.get("/api/products", async (req,res) => {
+    
+    try {
+        const products = await Product.find({});
+        res.status(200).json({success:true, data:products});
+    } catch (error) {
+        console.error("Error in fetching products:", error.message);
+        res.status(500).json({success:false, message: "Server Error"});
+    }
+});
 
 app.post("/api/products", async (req,res) => {
     const product = req.body; //user will send this data
@@ -28,8 +40,35 @@ app.post("/api/products", async (req,res) => {
     }
 });
 
-//
+app.put("/api/products/:id", async (req, res) =>{
+    const {id} = req.params;
 
+    const product = req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+       return res.status(404).json({success:false, message:"Invalid Product Id"});
+    }
+    
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true});
+        res.status(200).json({success:true, data:updatedProduct});
+    } catch (error) {
+        // console.error("Error in updating product:", error.message);
+        res.status(500).json({success:false, message: "Server Error"});
+    }
+})
+
+app.delete("/api/products/:id", async (req, res) =>{
+    const {id} = req.params;
+    
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success:true, message: "Product Deleted"});
+    } catch (error) {
+        console.error("Error in deleting product:", error.message);
+        res.status(404).json({success:false, message: "Product Not Found"});
+    }
+})
 
 app.listen(5000, () => {
     connectDB();
