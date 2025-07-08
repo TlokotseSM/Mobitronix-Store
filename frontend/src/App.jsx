56c1e7443680561fd4120e05475cd99ab11e839d
TlokotseSM <tlokotsemogudi@gmail.com>
2025-07-08 11:42:44 +0200

    Added Navbar and routes for Home & Create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
-import { Box, Button } from '@chakra-ui/react'
-import { Route } from 'react-router-dom'
+import { Box } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+
+import CreatePage from './pages/CreatePage';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
       <Box minH={"100vh"}>
-        {/* <NavBar></NavBar> */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/" element={<CreatePage />} />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<CreatePage />} />
+        </Routes>
       </Box>
     </>
   )
