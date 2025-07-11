12de8f027d04776cc16faf2c66c475df6a8e71ac
TlokotseSM <tlokotsemogudi@gmail.com>
2025-07-11 12:13:42 +0200

    updated CreatePage route path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,13 +11,13 @@ function App() {
     <>
       <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/" element={<CreatePage />} />
-        </Routes>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+				    <Route path='/create' element={<CreatePage />} />
+          </Routes>
       </Box>
     </>
-  )
+  );
 }
 
 export default App
