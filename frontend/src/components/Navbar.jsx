d31590f9467722970edb880f37138e3207df4ede
TlokotseSM <tlokotsemogudi@gmail.com>
2025-07-29 16:03:50 +0200

    updated my product store UI name

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
         bgGradient={"linear(to-r, cyan.400, blue.500)"}
         bgClip={"text"}
         >
-          <Link to={'/'}>Product Store 🛒</Link>
+          <Link to={'/'}>Tlokotse's Mobitronix Store 🛒</Link>
         
       </Text>
 
