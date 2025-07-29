c2a5936e2efa68f98cc8cba061c3f913670b6f22
TlokotseSM <tlokotsemogudi@gmail.com>
2025-07-29 15:50:52 +0200

    updated product prices to rands

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -93,7 +93,7 @@ const ProductCard = ({ product }) => {
 				</Heading>
 
 				<Text fontWeight='bold' fontSize='xl' color={textColor} mb={4}>
-					${product.price}
+					R{product.price}
 				</Text>
 
 				<HStack spacing={2}>
