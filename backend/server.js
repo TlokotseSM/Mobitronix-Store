dfb2e666a91b563e928410a7988ea3a2127829d4
TlokotseSM <tlokotsemogudi@gmail.com>
2025-08-25 12:43:30 +0200

    updated fallback route for React build and refresh lockfile

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,9 @@ app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+	app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 }
 
 app.listen(PORT, () => {
